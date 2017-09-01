7bce822405b2ad3e42cb190987e4b1a23f894573
Annette Maza <nettymaza@gmail.com>
2017-09-01 05:04:53 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,4 +15,4 @@ end
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta span.location-name").text
-# project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i

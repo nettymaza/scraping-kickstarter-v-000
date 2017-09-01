d4dad9570a2f16006a46f7c7cc5ef5845a10d8b5
Annette Maza <nettymaza@gmail.com>
2017-09-01 05:01:54 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,7 +6,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
-  
+
 
 end
 
@@ -14,5 +14,5 @@ end
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
-# location: project.css("location-name").text
+# location: project.css("ul.project-meta li.a.span.location-name").text
 # project.css("ul.project-stats li.first.funded strong").text
